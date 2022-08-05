b7e13f054ff33b3f223a2d16f1feb66df4af588a
Viktoriia Doroshenko <dorvika2@gmail.com>
2022-08-05 12:41:39 +0300

    basic files structure & routing

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Header, Footer } from "./components";
+import Router from "./router/Router";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Router />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
