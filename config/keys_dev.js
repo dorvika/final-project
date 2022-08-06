469ba9557bd8941e941a1ca9ffcfb52666eeada1
Viktoriia Doroshenko <dorvika2@gmail.com>
2022-08-06 13:02:28 +0300

    removed server folder

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dorvika:2408vd@cluster0.hd332.mongodb.net/postil?retryWrites=true&w=majority",
+  secretOrKey: "random very very secret string",
+};
--- server/config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://dorvika:2408vd@cluster0.hd332.mongodb.net/?retryWrites=true&w=majority",
-  secretOrKey: "random very very secret string",
-};
