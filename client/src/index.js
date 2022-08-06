bac7c44e67a9768ac91e5a2c13b9eea7fc930cee
Valentyn <Valentine@Yulias-iMac.local>
2022-08-06 21:03:37 +0300

    added ThemeProvider/theme

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,18 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./muiTheme/theme";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
