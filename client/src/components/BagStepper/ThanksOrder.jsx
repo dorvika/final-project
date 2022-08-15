d13fc75dbe8e229e0e7ee394bdb1ef466fe79646
Valentyn <Valentine@Yulias-iMac.local>
2022-08-15 21:37:10 +0300

    fix ThanksOrder page

--- client/src/components/BagStepper/ThanksOrder.jsx
+++ client/src/components/BagStepper/ThanksOrder.jsx
@@ -5,14 +5,23 @@ import React from "react";
 const ThanksOrder = () => {
   return (
     <React.Fragment>
-      <Box sx={{display:"flex", flexDirection: "column", justifyContent: "center", alignItems:"center", padding:"150px 0"}}>
-        <Typography variant="h2" sx={{ mb: "50px", textTransform: "uppercase"}}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "150px 0",
+        }}
+      >
+        <Typography
+          variant="h2"
+          sx={{ mb: "50px", textTransform: "uppercase" }}
+        >
           Thank you for your order!
         </Typography>
-        <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-          <Box sx={{ flex: "1 1 auto" }} />
-          <Button sx={{p:"15px 85px"}}>Continue Shopping</Button>
-        </Box>
+
+        <Button sx={{ p: "15px 85px" }}>Continue Shopping</Button>
       </Box>
     </React.Fragment>
   );
