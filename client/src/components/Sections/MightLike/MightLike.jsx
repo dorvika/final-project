b729055ff26edd7f0ab6da671f266689bdfa5055
Vlad <vladonish@gmail.com>
2022-08-28 13:32:56 +0300

    implement ligin for a MightLike cards slider

--- client/src/components/Sections/MightLike/MightLike.jsx
+++ client/src/components/Sections/MightLike/MightLike.jsx
@@ -1,9 +1,36 @@
+import './style.css'
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 
-const MightLike = () => {
+const MightLike = ({SectionTitle = "Related Items"}) => {
+  const slides = [1,2,3,4,5,6]
+
+  const slideLeft = () => {
+    const slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 380
+  }
+  const slideRight = () => {
+    const slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 380
+  }
+
   return (
-    <div>
-      Might Like
-    </div>
+    <section id="body">
+      <h1>{SectionTitle}</h1>
+      <div id="main-slider-container">
+        <KeyboardArrowLeftIcon sx={{ width: "40px", height: "40px" }} className="slider-icon left" onClick={slideLeft}/>
+        <div id="slider">
+          {slides.length > 0 ?
+            <>
+              {slides.map((el,index) => (
+                <div className="slider-card" key={index}>{el}</div>
+              ))}
+            </>
+            : <p>Ups :(</p>}
+        </div>
+        <KeyboardArrowRightIcon sx={{ width: "40px", height: "40px" }} className="slider-icon right" onClick={slideRight}/>
+      </div>
+    </section>
   );
 };
 
---
+++ client/src/components/Sections/MightLike/data.js
@@ -0,0 +1,33 @@
+export const dataElements = [
+  {
+    id: 1,
+    title: "Boho Pink Cotton Bed Lilen",
+    urlImage: "https://res.cloudinary.com/dyvsyavmb/image/upload/v1661614471/iptzvtzf9x5onhfmr5mz.jpgg",
+    price: "230"
+  },{
+    id: 2,
+    title: "Almond Cotton Bed Lilen",
+    urlImage: "https://res.cloudinary.com/dyvsyavmb/image/upload/v1661614457/cisqqbnb65ugeollkneu.jpg",
+    price: "245"
+  },{
+    id: 3,
+    title: "Boho Creame Cotton Bed Lilen",
+    urlImage: "https://res.cloudinary.com/dyvsyavmb/image/upload/v1661614465/ehyqz8oagpd1l0qfvije.jpg",
+    price: "180"
+  },{
+    id: 4,
+    title: "Mint Candy Bed Lilen",
+    urlImage: "https://res.cloudinary.com/dyvsyavmb/image/upload/v1661614485/bhzroxyvnhoavywx7vdi.jpg",
+    price: "155"
+  },{
+    id: 5,
+    title: "Orange Candy Bed Lilen",
+    urlImage: "https://res.cloudinary.com/dyvsyavmb/image/upload/v1661614491/vhbnusdjo2vaj7ufrdde.png",
+    price: "175"
+  },{
+    id: 6,
+    title: "Geometric Bed Lilen",
+    urlImage: "https://res.cloudinary.com/dyvsyavmb/image/upload/v1661614477/jcxjkccos8kpdqd3yfv3.jpg",
+    price: "150"
+  }
+]
