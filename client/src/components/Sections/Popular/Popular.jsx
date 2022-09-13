dcfa4e3c5500ba1944b81c35c5774e7564306ad1
Roman Organ <r.organ@dan-it.com.ua>
2022-09-13 18:57:44 +0300

    remade logic. removed products and keep only categories with images

--- client/src/components/Sections/Popular/Popular.jsx
+++ client/src/components/Sections/Popular/Popular.jsx
@@ -9,9 +9,6 @@ import {
   useMediaQuery,
 } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Preloader } from "../../../pages/Categories";
-import { fetchProducts } from "../../../store/Products/actions";
 import { fetchData } from "../../../utils/api";
 import {
   CustomButton,
@@ -23,27 +20,14 @@ import {
 
 const Popular = () => {
   const matches = useMediaQuery("(max-width: 899px)");
-  const dispatch = useDispatch();
   const [categories, setCategories] = useState([]);
-  const { products, isLoading, hasError } = useSelector(
-    (state) => state.products
-  );
 
   useEffect(() => {
     fetchData("/catalog").then((res) => setCategories(res));
   }, []);
 
-  useEffect(() => {
-    dispatch(fetchProducts());
-  }, []);
-
   const shuffledCategories = categories.sort(() => 0.5 - Math.random());
   const categoriesToShow = shuffledCategories.slice(0, 4);
-  const shuffledProducts = products.sort(() => 0.5 - Math.random());
-
-  const uniqProducts = categoriesToShow.map((el) =>
-    shuffledProducts.find((product) => product.categories === el.name)
-  );
 
   return (
     <Box
@@ -65,45 +49,37 @@ const Popular = () => {
         Popular
       </Typography>
 
-      {hasError && (
-        <div>
-          <p>Ooops, something went wrong</p>
-        </div>
-      )}
-      {isLoading ? (
-        <Preloader style={{ marginBottom: "15px" }} />
-      ) : (
-        <Grid container spacing={10} sx={{ flexGrow: 1 }}>
-          {uniqProducts.map((product, index) => (
-            <CustomGridItem
-              item
-              key={product.item}
-              xs={matches ? 12 : index === 0 || index === 3 ? 8 : 4}
+      <Grid container spacing={10} sx={{ flexGrow: 1 }}>
+        {categoriesToShow.map((category, index) => (
+          <CustomGridItem
+            item
+            key={category.id}
+            xs={matches ? 12 : index === 0 || index === 3 ? 8 : 4}
+          >
+            <Link
+              href={`/catalog?categories=${category.name}&perPage=9&startPage=1`}
             >
-              <Link
-                href={`/catalog?categories=${product.categories}&perPage=9&startPage=1`}
-              >
-                <Card sx={{ maxWidth: "100%" }}>
-                  <CardMedia
-                    component="img"
-                    height="340"
-                    image={product.imageUrls[0]}
-                    alt="popular-img"
-                  />
-                  <CustomCardContent sx={{ paddingRight: "20px" }}>
-                    <CardContent sx={{ paddingRight: "45px" }}>
-                      <CustomTypography variant="p" component="p">
-                        {product.categories}
-                      </CustomTypography>
-                      <CustomButton>Shop</CustomButton>
-                    </CardContent>
-                  </CustomCardContent>
-                </Card>
-              </Link>
-            </CustomGridItem>
-          ))}
-        </Grid>
-      )}
+              <Card sx={{ maxWidth: "100%" }}>
+                <CardMedia
+                  component="img"
+                  height="340"
+                  image={category.imgUrl}
+                  alt="popular-img"
+                />
+                <CustomCardContent sx={{ paddingRight: "20px" }}>
+                  <CardContent sx={{ paddingRight: "45px" }}>
+                    <CustomTypography variant="p" component="p">
+                      {category.name}
+                    </CustomTypography>
+                    <CustomButton>Shop</CustomButton>
+                  </CardContent>
+                </CustomCardContent>
+              </Card>
+            </Link>
+          </CustomGridItem>
+        ))}
+      </Grid>
+      {/* )} */}
 
       <CustomButton sx={{ mt: "30px", padding: "0" }}>
         <Link
