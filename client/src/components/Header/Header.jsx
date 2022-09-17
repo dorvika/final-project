69d9ec230379ab59cc0837b563bba2a98e73b786
Viktoriia Doroshenko <dorvika2@gmail.com>
2022-09-17 12:00:22 +0300

    deleted price ever

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,106 +3,131 @@ import { openModal } from "../../store/Modal/actions";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import {
-    Typography,
-    Container,
-    Box,
-    InputAdornment,
-    IconButton,
-    Divider,
-    Badge,
+  Typography,
+  Container,
+  Box,
+  InputAdornment,
+  IconButton,
+  Divider,
+  Badge,
 } from "@mui/material";
-import {HeaderInput, HeaderLinks, IconsButtonContainer} from "./components/styles";
+import {
+  HeaderInput,
+  HeaderLinks,
+  IconsButtonContainer,
+} from "./components/styles";
 import Catalog from "./components/catalogButton.jsx";
 import Logo from "./components/logoSvg";
 import {
-    FavoriteBorderOutlined,
-    PersonOutlined,
-    Search,
+  FavoriteBorderOutlined,
+  PersonOutlined,
+  Search,
 } from "@mui/icons-material";
 
 import BagPopper from "./components/BagPopper.jsx";
 import LoginPopper from "./components/LoginPopper.jsx";
-
+import { useEffect, useMemo, useState } from "react";
+import { debounce } from "lodash";
+import { postSearchQuery } from "../../utils/api.js";
 
 const Header = () => {
-    const dispatch = useDispatch();
-    const { isLoggedIn } = useSelector((state) => state.loggedIn);
-    const modal = useSelector((state) => state.modal);
-    const favorites = useSelector((state) => state.favorites.favorites);
+  const dispatch = useDispatch();
+  const { isLoggedIn } = useSelector((state) => state.loggedIn);
+  const modal = useSelector((state) => state.modal);
+  const favorites = useSelector((state) => state.favorites.favorites);
+  const [query, setQuery] = useState("");
 
+  const changeHandler = (event) => {
+    setQuery(event.target.value);
+  };
 
-    const handleOpen = () => {
-        dispatch(openModal());
+  const debouncedChangeHandler = useMemo(
+    () => debounce(changeHandler, 300),
+    []
+  );
+
+  useEffect(() => {
+    // postSearchQuery("products/search", {query}).then((data) =>
+    //   console.log(data)
+    // );
+  }, [query]);
+
+  useEffect(() => {
+    return () => {
+      debouncedChangeHandler.cancel();
     };
-    return (
-      <header>
-        <Container
+  }, []);
+
+  const handleOpen = () => {
+    dispatch(openModal());
+  };
+  return (
+    <header>
+      <Container
+        sx={{
+          fontSize: "16px",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Link style={{ marginLeft: "-2.6041666666666665vw" }} to={"/"}>
+          <Logo></Logo>
+        </Link>
+        <Catalog />
+
+        <HeaderLinks>
+          <Link to="/aboutus" style={{ textDecoration: "none" }}>
+            <Typography variant="body">About</Typography>
+          </Link>
+          <Link to="/contact" style={{ textDecoration: "none" }}>
+            <Typography variant="body">Contact</Typography>
+          </Link>
+          <Link to="/blog" style={{ textDecoration: "none" }}>
+            <Typography variant="body">Blog</Typography>
+          </Link>
+        </HeaderLinks>
+        <Box>
+          <HeaderInput
+            endAdornment={
+              <InputAdornment position={"start"}>
+                <Search></Search>
+              </InputAdornment>
+            }
+            id="standard-basic"
+            placeholder="Search"
+            variant="standard"
+            onChange={debouncedChangeHandler}
+          />
+        </Box>
+        <IconsButtonContainer
           sx={{
-            fontSize: "16px",
             display: "flex",
             justifyContent: "space-between",
             alignItems: "center",
+            width: "10%",
           }}
         >
-          <Link style={{ marginLeft: "-2.6041666666666665vw" }} to={"/"}>
-            <Logo></Logo>
-          </Link>
-          <Catalog />
-
-          <HeaderLinks>
-            <Link to="/aboutus" style={{ textDecoration: "none" }}>
-              <Typography variant="body">About</Typography>
-            </Link>
-            <Link to="/contact" style={{ textDecoration: "none" }}>
-              <Typography variant="body">Contact</Typography>
-            </Link>
-            <Link to="/blog" style={{ textDecoration: "none" }}>
-              <Typography variant="body">Blog</Typography>
-            </Link>
-          </HeaderLinks>
-          <Box>
-            <HeaderInput
-              endAdornment={
-                <InputAdornment position={"start"}>
-                  <Search></Search>
-                </InputAdornment>
-              }
-              id="standard-basic"
-              placeholder="Search"
-              variant="standard"
-            />
-          </Box>
-          <IconsButtonContainer
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              width: "10%",
-            }}
-          >
-            {isLoggedIn ? (
-              <LoginPopper />
-            ) : (
-              <IconButton onClick={handleOpen}>
-                <PersonOutlined sx={{ color: "primary.main" }} />
-              </IconButton>
-            )}
-
-            <IconButton
-              sx={{ color: "primary.main" }}
-              href={"/favorites"}
-            >
-              <Badge badgeContent={favorites.length} color="error">
-                <FavoriteBorderOutlined />
-              </Badge>
+          {isLoggedIn ? (
+            <LoginPopper />
+          ) : (
+            <IconButton onClick={handleOpen}>
+              <PersonOutlined sx={{ color: "primary.main" }} />
             </IconButton>
-            <BagPopper />
-          </IconsButtonContainer>
-          {modal && <Authorization />}
-        </Container>
-        <Divider sx={{ borderColor: "primary.main", mb: "20px" }} />
-      </header>
-    );
+          )}
+
+          <IconButton sx={{ color: "primary.main" }} href={"/favorites"}>
+            <Badge badgeContent={favorites.length} color="error">
+              <FavoriteBorderOutlined />
+            </Badge>
+          </IconButton>
+          <BagPopper />
+        </IconsButtonContainer>
+        {modal && <Authorization />}
+      </Container>
+      <Divider sx={{ borderColor: "primary.main", mb: "20px" }} />
+    </header>
+  );
 };
 
 export default Header;
