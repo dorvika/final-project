b0e7fbe083eb56e7c978eff7e8819b56cd6278b5
Viktoriia Doroshenko <dorvika2@gmail.com>
2022-09-18 16:21:36 +0300

    search page finished

--- client/src/components/Header/components/SearchBar.jsx
+++ client/src/components/Header/components/SearchBar.jsx
@@ -1,36 +1,46 @@
 import { Search } from "@mui/icons-material";
-import { Box, InputAdornment, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
+import { Box, InputAdornment, Stack, Typography } from "@mui/material";
+import { useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setSearchedProducts } from "../../../store/Filters/actions";
 import { postSearchQuery } from "../../../utils/api";
 import SearchProductCard from "./SearchProductCard.jsx";
 import { CustomDialog, CustomLink, HeaderInput } from "./styles";
+import Preloader from "../../../pages/Categories/Catalog Product List/Preloader.jsx";
+import { debounce } from "lodash";
 
 const SearchBar = () => {
   const dispatch = useDispatch();
   const { searchedProducts } = useSelector((state) => state.filters);
   const [searchPhrase, setSearchPhrase] = useState("");
   const [open, setOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleClose = () => {
     setOpen(false);
   };
 
-  useEffect(() => {
-    if (searchPhrase) {
-      postSearchQuery("products/search", { query: searchPhrase }).then(
-        ({ data }) => {
-          dispatch(setSearchedProducts(data));
-        }
-      );
+  const handleChange = (event) => {
+    setSearchPhrase(event.target.value);
+  };
+
+  const handleFetch = (input) => {
+    if (input) {
+      setIsLoading(true);
+      postSearchQuery("products/search", { query: input }).then(({ data }) => {
+        dispatch(setSearchedProducts(data));
+        setIsLoading(false);
+      });
     }
-  }, [searchPhrase]);
+  };
+
+  const debouncedFetch = useMemo(() => debounce(handleFetch, 300), []);
 
   const showResults = (e) => {
     if (e.key === "Enter") {
       setOpen(true);
       setSearchPhrase("");
+      debouncedFetch(searchPhrase);
     }
   };
 
@@ -56,29 +66,37 @@ const SearchBar = () => {
           </InputAdornment>
         }
         placeholder="Type and press Enter to search..."
-        onChange={(e) => setSearchPhrase(e.target.value)}
+        onChange={handleChange}
         onKeyDown={showResults}
         value={searchPhrase}
       />
       <CustomDialog onClose={handleClose} open={open}>
-        {!searchedProducts.length && (
+        {isLoading && (
+          <Stack
+            justifyContent="center"
+            sx={{ minWidth: "400px", minHeight: "100px" }}
+          >
+            <Preloader />
+          </Stack>
+        )}
+        {!isLoading && searchProductCard}
+        {searchedProducts.length > 5 && !isLoading && (
+          <CustomLink
+            to="/search"
+            onClick={handleClose}
+            style={{ textDecoration: "none" }}
+          >{`Show All (${searchedProducts.length})`}</CustomLink>
+        )}
+        {!searchedProducts.length && !isLoading && (
           <Box p="15px" sx={{ textAlign: "center" }}>
             <Typography
               variant="h2"
               sx={{ lineHeight: 1.2, letterSpacing: 0, fontSize: 24 }}
             >
               Sorry, but nothing was found according to your request. Please,
-              try again
+              try again.
             </Typography>
           </Box>
-        )}
-        {searchProductCard}
-        {searchedProducts.length > 5 && (
-          <CustomLink
-            to="/search"
-            onClick={handleClose}
-            style={{ textDecoration: "none" }}
-          >{`Show All (${searchedProducts.length})`}</CustomLink>
         )}
       </CustomDialog>
     </>
--- client/src/pages/Search/Search.jsx
+++ client/src/pages/Search/Search.jsx
@@ -1,14 +1,54 @@
-import { Container, Typography } from "@mui/material";
+import { Box, Container, Link, Stack, Typography } from "@mui/material";
+import Masonry from "@mui/lab/Masonry";
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { CustomSearchTypography } from "./styles";
 
 const Search = () => {
   const { searchedProducts } = useSelector((state) => state.filters);
-  console.log(searchedProducts);
+  const searchProductsToShow = searchedProducts.map((item) => (
+    <div
+      key={item.itemNo}
+      style={{
+        position: "relative",
+        boxShadow: "0px 1px 1px rgba(0, 0, 0, 0.25)",
+      }}
+    >
+      <Link href={`/catalog/${item.itemNo}`}>
+        <CustomSearchTypography variant="h2">
+          <div>{item.name}</div>
+          <div style={{ fontSize: "32px" }}>${item.currentPrice}</div>
+          <div>{item.size}</div>
+        </CustomSearchTypography>
+
+        <img
+          src={`${item.imageUrls[0]}`}
+          srcSet={`${item.imageUrls[0]}`}
+          alt={item.name}
+          loading="lazy"
+          style={{
+            display: "block",
+            width: "100%",
+          }}
+        />
+      </Link>
+    </div>
+  ));
+
   return (
-    <Container>
-      {!searchedProducts.length && <Navigate to="/" />}
-      <Typography variant="h2">Search</Typography>
+    <Container sx={{ mb: "10px" }}>
+      {!searchedProducts.length && (
+        <Stack justifyContent="center" sx={{ minHeight: "450px" }}>
+          <Typography variant="h2" textAlign="center" marginTop="-60px">
+            Sorry, but nothing was found according to your request.
+            <div>Please, try again.</div>
+          </Typography>
+        </Stack>
+      )}
+      <Box sx={{ width: "100%" }}>
+        <Masonry columns={3} spacing={6}>
+          {searchProductsToShow}
+        </Masonry>
+      </Box>
     </Container>
   );
 };
