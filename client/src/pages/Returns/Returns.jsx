be285e96d0ad4289668c608042da8d627fc0f805
Viktoriia Doroshenko <dorvika2@gmail.com>
2022-09-23 20:50:34 +0300

    added returns page

--- client/src/pages/Returns/Returns.jsx
+++ client/src/pages/Returns/Returns.jsx
@@ -1,5 +1,50 @@
+import { Container, Stack, Typography } from "@mui/material";
+import { data } from "./data";
+import { CustomReturnsBox, CustomReturnsButton } from "./styles";
+
 const Returns = () => {
-  return <div>Returns</div>;
+  const returnsItems = data.map(({ id, title, list, btnText }) => (
+    <Stack width={{ xs: "100%", sm: "50%" }} alignItems="flex-start" key={id}>
+      <Typography variant="h3" fontWeight={600} mb="20px" mt="10px">
+        {title}
+      </Typography>
+      {list.map((item, index) => (
+        <Typography mb="5px" key={index}>
+          {item}
+        </Typography>
+      ))}
+      <CustomReturnsButton>{btnText}</CustomReturnsButton>
+    </Stack>
+  ));
+
+  return (
+    <>
+      <CustomReturnsBox>
+        <Typography color="white" mb="20px">
+          365 DAY GUARANTEE
+        </Typography>
+        <Typography
+          color="white"
+          mb="20px"
+          variant="h4"
+          textTransform="uppercase"
+        >
+          Free Returns
+        </Typography>
+        <Typography width="80%" color="white" mb="20px">
+          Return any item* within 30 days for a full merchandise refund or
+          return within 1 year of purchase for store credit equal to the
+          original value of the item(s). Even if it&apos;s been washed or worn.
+        </Typography>
+        <CustomReturnsButton>START RETURN</CustomReturnsButton>
+      </CustomReturnsBox>
+      <Container sx={{ mb: "20px" }}>
+        <Stack direction={{ xs: "column", sm: "row" }} gap="15px">
+          {returnsItems}
+        </Stack>
+      </Container>
+    </>
+  );
 };
 
 export default Returns;
