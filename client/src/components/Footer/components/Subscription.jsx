d06f5d12341bdd93df0654536868afaaf8a254f1
Roman Organ <r.organ@dan-it.com.ua>
2022-09-27 17:18:29 +0300

    fix subscribe in footer for 320px screen

--- client/src/components/Footer/components/Subscription.jsx
+++ client/src/components/Footer/components/Subscription.jsx
@@ -79,9 +79,14 @@ const Subscription = () => {
               variant="contained"
               type="submit"
               disabled={props.isSubmitting}
-              sx={{
-                p: "5px 40px",
-              }}
+              sx={(theme) => ({
+                [theme.breakpoints.up("sm")]: {
+                  p: "5px 40px",
+                },
+                [theme.breakpoints.down("sm")]: {
+                  p: "5px 25px",
+                },
+              })}
             >
               Send
             </Button>
